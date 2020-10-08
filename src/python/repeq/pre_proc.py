4026a52844bde0fda65b9a0e506cd141a233979a
JTim <jiuntim.lin@gmail.com>
2020-10-08 12:46:28 -0700

    add pre_proc

--- src/python/repeq/pre_proc.py
+++ src/python/repeq/pre_proc.py
@@ -2,14 +2,24 @@
 
 import glob
 import obspy
+from obspy import UTCDateTime
 
 
-
-def merge_daily(home,project_name):
+def merge_daily(home,project_name,sampling_rate,filter=[0.2,8]):
     Ds = glob.glob(home+'/'+project_name+'/waveforms/'+'*000000')
     Ds.sort()
     print('Number of dirs=%d'%(len(Ds)))
     for D in Ds:
         print('In dir:',D)
-
+        t1 = UTCDateTime(D.split('/')[-1])
+        t2 = t1 + 86400 #this is daily so exactly +86400 sec
+        files = glob.glob(D+'/waveforms/*.mseed')
+        st = obspy.read(files)
+        st.merge(method=1,interpolation_samples=-1,fill_value='interpolate')
+        st.detrend()
+        if filter:
+            st.filter("bandpass",freqmin=filter[0],freqmax=filter[1])
+        st.trim(starttime=t1-2, endtime=t2+2, nearest_sample=1, pad=1, fill_value=0)
+        st.interpolate(sampling_rate=sr, starttime=t1)
+        st.trim(starttime=t1, endtime=t2, nearest_sample=1, pad=1, fill_value=0)
 
