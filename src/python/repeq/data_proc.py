5798329776d1bc05315cc112597edb952c8797a3
JTim <jiuntim.lin@gmail.com>
2020-11-17 16:30:23 -0800

    clean up cal_lag 1

--- src/python/repeq/data_proc.py
+++ src/python/repeq/data_proc.py
@@ -602,7 +602,7 @@ def cal_lag(template,daily_cut,tcs_length_temp,tcs_length_daily,align_wind,measu
 #        print('daily data from:',D_daily.stats.starttime,D_daily.stats.endtime)
 #        print('Trim st=',t_st_daily-2)
         D_daily.trim(starttime=t_st_daily-2,endtime=t_ed_daily+2,nearest_sample=1, pad=1, fill_value=0)
-        D_daily = D_daily.copy()
+        D_daily = D_daily.slice(starttime=t_st_daily-2,endtime=t_ed_daily+2)
 #        print('After trim, daily data from:',D_daily.stats.starttime,D_daily.stats.endtime)
         #interpolate data
 #        print('orig_data=',daily_cut)
