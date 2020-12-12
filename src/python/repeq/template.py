c2a0e91363449f8b6b0aa3c543667b8553e9b24e
JTim <jiuntim.lin@gmail.com>
2020-12-12 00:17:30 -0800

    parallelize xcorr calculation test 2

--- src/python/repeq/template.py
+++ src/python/repeq/template.py
@@ -449,7 +449,7 @@ def T_partition(T,n_part=4,save_CCF=False,fmt=2):
     #create a function to be parallel
     #normally to run code: sav_TT[i].xcorr_cont(save_CCF=False,fmt=1), loop the i
     def run_loop(i):
-        sav_TT[i].xcorr_cont(save_CCF=False,fmt=fmt)
+        sav_TT[i].xcorr_cont(save_CCF=False,fmt=2)
 
     results = Parallel(n_jobs=n_part,verbose=10,backend='multiprocessing')(delayed(run_loop)(i) for i in range(n_part)  )
     print(results)
