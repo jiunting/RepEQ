9ec73802c822cd9e8e548ebe6ca7ff7113e02380
JTim <jiuntim.lin@gmail.com>
2021-01-02 13:48:19 -0800

    try manual colormap in data_visual.plot_reptcs 4

--- src/python/repeq/data_visual.py
+++ src/python/repeq/data_visual.py
@@ -231,11 +231,12 @@ def my_seismic():
     G1 = np.flipud(tmpRGB[:N//2,1])
     B1 = np.flipud(tmpRGB[:N//2,2])
 
-    tmp_seis = plt.cm.get_cmap('spring',N//2) #concate whole spring
-    tmpRGB = tmp_seis(range(N//2))
-    R2 = tmpRGB[:,0]
-    G2 = tmpRGB[:,1]
-    B2 = tmpRGB[:,2]
+    #tmp_seis = plt.cm.get_cmap('spring',N//2) #concate whole spring
+    tmp_seis = plt.cm.get_cmap('hsv',N) #concate hsv begin half
+    tmpRGB = tmp_seis(range(N))
+    R2 = tmpRGB[:N//2,0]
+    G2 = tmpRGB[:N//2,1]
+    B2 = tmpRGB[:N//2,2]
 #    R2 = tmpRGB[N//2:,0]
 #    G2 = tmpRGB[N//2:,1]
 #    B2 = tmpRGB[N//2:,2]
